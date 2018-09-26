eb58be614f9314efc6780d2304d0ef23a4b2f10a
carlos <carlos@krasamo.com>
2018-09-26 12:40:02 -0500

    Complete main screen

--- app/src/main/java/com/crodr/bakingapp/ui/MainActivity.java
+++ app/src/main/java/com/crodr/bakingapp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.crodr.bakingapp;
+package com.crodr.bakingapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.crodr.bakingapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.mainScreenFrame, RecipesFragment.newInstance()).commit();
+        }
     }
 }
